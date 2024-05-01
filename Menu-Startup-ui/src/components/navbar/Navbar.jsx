25cc0e749254f5c9f53a1ae4c17bc460f0dddce9
lbryant-sss <brianmungai350@gmail.com>
2024-05-01 17:27:51 +0300

    Nav Responsive

--- Menu-Startup-ui/src/components/navbar/Navbar.jsx
+++ Menu-Startup-ui/src/components/navbar/Navbar.jsx
@@ -5,34 +5,48 @@ import { FaBars } from "react-icons/fa";
 import { useState } from "react";
 
 const Navbar = () => {
-    const [showNav, setShowNav] = useState(false);
+
+    function showSidebar(){
+        const sidebar = document.querySelector('.navbar_links')
+        sidebar.style.display = 'flex'
+    }
+
+    function hideSidebar() {
+        const hidebar = document.querySelector('.navbar_links')
+        hidebar.style.display = 'none'
+    }
 
     return (
         <nav className="navbar side_navbar">
             <wrapper className="navbar_container side_nav_container">
-                <a href="#" className="navbar_logo" onClick={() => setShowNav(false)}>
+                <a href="#" className="navbar_logo">
                             <img src={Logo} alt="" />
                 </a>
-                <div className={'navbar_menubar $ {showNav ? "bg-color" : ""}'} onClick={() => setShowNav(!showNav)}>
-                    <FaBars />
-                </div>
+                
                 <ul className={'navbar_links $ {showNav ? "show-nav" : ""}'}>
-                    <li onClick={() => setShowNav(false)}>
-                        <a href="#">Home</a>
+                     <li onClick={hideSidebar} className="closebar">
+                        <a href="">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+                        </a>
+                     </li>
+                    <li>
+                        <a href="#">Homer</a>
                     </li>
-                    <li onClick={() => setShowNav(false)}>
+                    <li>
                         <a href="#">Pricing</a>
                     </li>
-                    <li onClick={() => setShowNav(false)}>
+                    <li>
                         <a href="#">How to set-up</a>
                     </li>
-                    <li onClick={() => setShowNav(false)}>
+                    <li>
                     <a href="" className="button-primary navbar_btn">
                     Sign Up
                 </a>
                     </li>
                 </ul>
-                
+                <div onClick={showSidebar} className={'navbar_menubar $ {showNav ? "bg-color" : ""}'}>
+                    <FaBars />
+                </div>
             </wrapper>
         </nav>
     )
