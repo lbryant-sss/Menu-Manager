cd17b44844c9f696ae48dc07d55d7c77f96f1f9b
lbryant-sss <brianmungai350@gmail.com>
2024-05-18 22:18:12 +0300

    React Components added with navigation

--- Menu-Startup-ui/src/App.jsx
+++ Menu-Startup-ui/src/App.jsx
@@ -2,13 +2,40 @@ import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Howto from "./components/howto/Howto";
 
+import { BrowserRouter, Routes, Navigate, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
 function App() {
   return (
-    <>
-     <Navbar />
-     <Hero />
-     <Howto />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<RegisterAndLogout />}/>
+        <Route path="*" element={<NotFound />}> </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
